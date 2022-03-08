ceb8a547c56b4dc30d45c7784a1f426c00421ab1
Frédéric Meyer <frederic.meyer.77@gmail.com>
2022-03-08 14:12:20 +0100

    Clean of test files

--- src/lib.rs
+++ src/lib.rs
@@ -1,23 +1,3 @@
 mod files;
 pub mod config;
 pub mod commands;
-
-pub fn show() -> std::io::Result<()> {
-    let filename = match std::env::args_os().skip(1).next() {
-        Some(name) => name,
-        None => {
-            let error = simple_error::SimpleError::new("No file specified");
-
-            return Err(std::io::Error::new(std::io::ErrorKind::Other, error));
-        }
-    };
-
-    let path = std::path::PathBuf::from(filename);
-    let src = files::source::RustSource::read(&path)?;
-
-    for t in src.translations {
-        println!("%> {:?}", t);
-    }
-
-    Ok(())
-}
